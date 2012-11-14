cc824b729a53c76a20111d65519c81a7612f7687
Reed Rizzo <reedling78@gmail.com>
2012-11-14 14:01:10 -0600

    init

--- public/Scripts/Models/Employee.js
+++ public/Scripts/Models/Employee.js
@@ -1,35 +1,11 @@
 o.Models.Employee = Backbone.Model.extend({
-	defaults: {
-		Employees : [
-			{name:'Tammie Babbitt',         id:1, teamid:1, jobid:2},
-			{name:'Elizabeth Karagitlieva', id:2, teamid:1, jobid:1},
-			{name:'David Mansch',           id:3, teamid:1, jobid:1},
-			{name:'Kevin Mech',             id:4, teamid:1, jobid:1},
-			{name:'Mike Biasi',             id:5, teamid:4, jobid:1},
-			{name:'Donna Gailus',           id:6, teamid:4, jobid:3},
-			{name:'Larry Gasik',            id:7, teamid:4, jobid:1},
-			{name:'Jeff Novak',             id:8, teamid:4, jobid:2},
-			{name:'Michael Shuck',          id:9, teamid:4, jobid:2},
-			{name:'Reed Rizzo',             id:10, teamid:3, jobid:2},
-			{name:'Rich Pesek',             id:11, teamid:3, jobid:2},
-		],
-		Teams: [
-			{name:'Design Team', id:1},
-			{name:'CMS', id:2},
-			{name:'ACG Team', id:3},
-			{name:'Universal Team', id:4}
-		],
-		Job: [
-			{name:'Designer', id:1},
-			{name:'Software Engineer', id:2},
-			{name:'Project Manager', id:3}
-		]
-	},
 	fetchData : function(callback){
 		console.log('boom');
 		
 		o.socket.on('static', function (data) {
-			console.log(data);
+			this.set('Employees')
+			this.set('Teams')
+			this.set('Job')
 			callback();
 		});
 	},
