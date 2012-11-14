0d80b983435381f370251f0599d84b32592e4bc8
Reed Rizzo <reedling78@gmail.com>
2012-11-14 16:47:05 -0600

    init

--- app.js
+++ app.js
@@ -38,10 +38,10 @@ io.sockets.on('connection', function (socket) {
 
 });
 
-var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-var redis = require("redis").createClient(rtg.port, rtg.hostname);
+// var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+// var redis = require("redis").createClient(rtg.port, rtg.hostname);
 
-redis.auth(rtg.auth.split(":")[1]); 
+// redis.auth(rtg.auth.split(":")[1]); 
 
 
 
