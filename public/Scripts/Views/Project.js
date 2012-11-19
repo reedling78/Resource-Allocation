9e4994822bd5fd77a6091d8cdb9666d85af84fca
Reed Rizzo <reedling78@gmail.com>
2012-11-19 14:47:32 -0600

    init

--- public/Scripts/Views/Project.js
+++ public/Scripts/Views/Project.js
@@ -28,7 +28,7 @@ o.Views.ProjectCollectionView = Backbone.View.extend({
 
 		//set gutter width
 		$('.Projects').css('width', $('.Calendar')[0].scrollWidth + 'px');
-
+console.log(projects); 
 		for (i = 0; i < projects.length; i++) {
 			projects[i].attributes = view.setDayInfo(projects[i].attributes);
 			console.log(projects[i].attributes); 
