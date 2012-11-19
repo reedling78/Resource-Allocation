b40c7da0b5cd20c8b9b8f6b9612eb2d99ece8984
Reed Rizzo <reedling78@gmail.com>
2012-11-19 15:25:06 -0600

    init

--- public/Scripts/Views/Project.js
+++ public/Scripts/Views/Project.js
@@ -71,21 +71,21 @@ o.Views.ProjectCollectionView = Backbone.View.extend({
 			startD = today;
 		} 
 		
-		//Calculate what day to start on
-		while (today.getTime() < startD.getTime()){
-			if(today.getDay() != 0 && today.getDay() != 6){
-				day++;
-			}
-			today.setDate(today.getDate()+1);
-		}
+		// //Calculate what day to start on
+		// while (today.getTime() < startD.getTime()){
+		// 	if(today.getDay() != 0 && today.getDay() != 6){
+		// 		day++;
+		// 	}
+		// 	today.setDate(today.getDate()+1);
+		// }
 
-		//Calculate how many day the project is set for
-		while (endD.getTime() != startD.getTime()){
-			if(startD.getDay() != 0 && startD.getDay() != 6){
-				duration++;
-			}
-			startD.setDate(startD.getDate()+1);
-		}
+		// //Calculate how many day the project is set for
+		// while (endD.getTime() != startD.getTime()){
+		// 	if(startD.getDay() != 0 && startD.getDay() != 6){
+		// 		duration++;
+		// 	}
+		// 	startD.setDate(startD.getDate()+1);
+		// }
 		
 		model.day = (isLessThenToday) ? 1 : day;
 		model.duration = duration;
