fc9f7d282c845ca4f821a7e782377072d7cd3427
Reed Rizzo <reedling78@gmail.com>
2012-11-19 14:07:13 -0600

    init

--- public/Scripts/Views/Project.js
+++ public/Scripts/Views/Project.js
@@ -29,8 +29,8 @@ o.Views.ProjectCollectionView = Backbone.View.extend({
 			view.currentHeadername = gutters[i].headername;
 		}
 
-		// //set gutter width
-		// $('.Projects').css('width', $('.Calendar')[0].scrollWidth + 'px');
+		//set gutter width
+		$('.Projects').css('width', $('.Calendar')[0].scrollWidth + 'px');
 
 		// for (i = 0; i < projects.length; i++) {
 		// 	projects[i].attributes = view.setDayInfo(projects[i].attributes);
