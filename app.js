1d728bdee42d59df8d02bb4685895db356846a66
Reed Rizzo <reedling78@gmail.com>
2012-11-20 10:23:15 -0600

    init

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ io.sockets.on('connection', function (socket) {
 	socket.emit('static', employeeData);
 
 	socket.on('save', function (data) { 
+		socket.emit('get projects');
 		socket.broadcast.emit('save', data);
 	});
 
