56cc1c23302947b552d3d51070898c0929fa319f
Reed Rizzo <reedling78@gmail.com>
2012-11-20 14:05:39 -0600

    init

--- serverjs/db.js
+++ serverjs/db.js
@@ -64,29 +64,29 @@ exports.updateProjects = function (client, projArray, callback) {
         + 'enddate = \'' + projArray[i].enddate + '\' '
         + 'WHERE id = ' + projArray[i].id;
 
-        console.log('-----0000-----0000-----0000-----0000');
-        console.log(s);
-        console.log('-----1111-----1111-----1111-----1111');
+        // console.log('-----0000-----0000-----0000-----0000');
+        // console.log(s);
+        // console.log('-----1111-----1111-----1111-----1111');
         statements.push(s);
         //console.log(s);
     };
 
     callback();
 
-// 	(function next() {
-//         var statement = statements.shift();
-//         if (statement) { 
-//           client.query(statement, function(err, response) {
-//             if (err) return callback(err);
-//                 console.dir(response);
-//             next();
-//           });
-//         }
-//         else{
-//             callback();
-//         }
+	(function next() {
+        var statement = statements.shift();
+        if (statement) { 
+          client.query(statement, function(err, response) {
+            if (err) return callback(err);
+                console.dir(response);
+            next();
+          });
+        }
+        else{
+            callback();
+        }
           
-//     })();
+    })();
 }
 
 
