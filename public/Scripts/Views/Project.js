ca5c518679e6fb26f0c8b3eefb462d5aea3bd246
Reed Rizzo <reedling78@gmail.com>
2012-11-20 20:44:43 -0600

    init

--- public/Scripts/Views/Project.js
+++ public/Scripts/Views/Project.js
@@ -126,7 +126,7 @@ o.Views.ProjectCollectionView = Backbone.View.extend({
 			})
 
 			$('.Projects ul ul').on('click', function(e){
-				console.log(e);
+				console.log(e.offsetX);
 			})
 
 			$('h6[contenteditable=true]').on('focusout', function(){
