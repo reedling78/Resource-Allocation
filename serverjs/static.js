ed65e1657e2a287c953714c86eddc01b9e1782ac
Reed Rizzo <reedling78@gmail.com>
2012-11-21 16:01:50 -0600

    init

--- serverjs/static.js
+++ serverjs/static.js
@@ -5,23 +5,53 @@ exports.klt = function () {
 
 	return {
 		Employees : [
-			{name:'Tammie Babbitt',         id:1, teamid:1, jobid:2},
-			{name:'Elizabeth Karagitlieva', id:2, teamid:1, jobid:1},
-			{name:'David Mansch',           id:3, teamid:1, jobid:1},
-			{name:'Kevin Mech',             id:4, teamid:1, jobid:1},
-			{name:'Mike Biasi',             id:5, teamid:4, jobid:1},
-			{name:'Donna Gailus',           id:6, teamid:4, jobid:3},
-			{name:'Larry Gasik',            id:7, teamid:4, jobid:1},
-			{name:'Jeff Novak',             id:8, teamid:4, jobid:2},
-			{name:'Michael Shuck',          id:9, teamid:4, jobid:2},
-			{name:'Reed Rizzo',             id:10, teamid:3, jobid:2},
+			{name:'Kevin Mech',             id:1, teamid:1, jobid:1},
+			{name:'Tammie Babbitt',         id:2, teamid:1, jobid:2},
+			{name:'Elizabeth Karagitlieva', id:3, teamid:1, jobid:1},
+			{name:'David Mansch',           id:4, teamid:1, jobid:1},
+			
+			{name:'Michael Shuck',          id:5, teamid:4, jobid:2},
+			{name:'Mike Biasi',             id:6, teamid:4, jobid:1},
+			{name:'Donna Gailus',           id:7, teamid:4, jobid:3},
+			{name:'Larry Gasik',            id:8, teamid:4, jobid:1},
+			{name:'Jeff Novak',             id:9, teamid:4, jobid:2},
+			
+			{name:'Chris Grove',            id:10, teamid:3, jobid:2},
 			{name:'Rich Pesek',             id:11, teamid:3, jobid:2},
+			{name:'Bob Springer',           id:12, teamid:3, jobid:2},
+			{name:'Reed Rizzo',             id:13, teamid:3, jobid:2},
+			{name:'Geoff Skyles',           id:14, teamid:3, jobid:2},
+			{name:'Ben Munge',              id:15, teamid:3, jobid:2}, 
+			{name:'Brent Panther',          id:16, teamid:3, jobid:2},
+			{name:'Eric Figura',            id:17, teamid:3, jobid:2},
+			{name:'Brian Layman',           id:18, teamid:3, jobid:2},
+			
+			{name:'Brian Pautsch',          id:19, teamid:2, jobid:2},
+			{name:'Dan Strabbing',          id:20, teamid:2, jobid:2},
+			{name:'Kevin Howe',          	id:21, teamid:2, jobid:2},
+			{name:'Mary Wenzel',          	id:22, teamid:2, jobid:2},
+			{name:'Andrew Schwantes',       id:23, teamid:2, jobid:2},
+			{name:'Anthony Avallone',       id:24, teamid:2, jobid:2},
+			{name:'Brian West',          	id:25, teamid:2, jobid:2},
+			{name:'Jon Kimball',            id:26, teamid:2, jobid:2},
+			{name:'Keith Franklin',         id:27, teamid:2, jobid:2},
+
+			{name:'Chris Pautsch',          id:28, teamid:5, jobid:2},
+			{name:'Betsy Kraft',            id:29, teamid:5, jobid:2},
+			{name:'Mike Hernandez',         id:30, teamid:5, jobid:2},
+
+			{name:'Erica Magda',            id:31, teamid:6, jobid:2},
+			{name:'Mike Mikulecky',         id:32, teamid:6, jobid:2},
+
+
 		],
 		Teams: [
 			{name:'Design Team', id:1},
-			{name:'CMS', id:2},
+			{name:'CMS Team', id:2},
 			{name:'ACG Team', id:3},
-			{name:'Universal Team', id:4}
+			{name:'Universal Team', id:4},
+			{name:'Marketing Team', id:5},
+			{name:'iTravel', id:6}
 		],
 		Job: [
 			{name:'Designer', id:1},
