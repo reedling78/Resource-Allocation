e2e38ed45e0ddeb7986c1f4e8d7e9230308f0d4b
Reed Rizzo <reedling78@gmail.com>
2012-11-30 15:52:40 -0600

    klt version

--- public/Scripts/main.js
+++ public/Scripts/main.js
@@ -8,14 +8,14 @@ window.o.isEditing = false;
 window.o.CONST = {};
 window.o.CONST.ServiceURL = 'http://klt.rizzonet.com';
 window.o.CONST.url = document.URL;
-window.o.CONST.cookie = function("kltra"){
+window.o.CONST.cookie = function(){
     var i,x,y,ARRcookies=document.cookie.split(";");
     for (i=0;i<ARRcookies.length;i++)
     {
         x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
         y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
         x=x.replace(/^\s+|\s+$/g,"");
-        if (x==c_name)
+        if (x=='kltra')
         {
             return unescape(y);
         }
@@ -53,5 +53,5 @@ if(o.CONST.url.indexOf('login') !== -1){
     } else {
         window.location = "/login"
     }
-    
+
 }
