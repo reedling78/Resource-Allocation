0c4fd3f2bf8062318113ad1c7024236a15380512
Reed Rizzo <reedling78@gmail.com>
2012-12-01 14:07:58 -0600

    klt version

--- public/Scripts/main.js
+++ public/Scripts/main.js
@@ -6,53 +6,54 @@ window.o.models = {};
 window.o.isEditing = false;
 
 window.o.CONST = {};
-window.o.CONST.ServiceURL = 'http://klt.rizzonet.com';
+window.o.CONST.ServiceURL = 'http://ra.keylimetie.com';
 window.o.CONST.url = document.URL;
 window.o.CONST.cookie = function () {
     'use strict';
     var i, x, y, ARRcookies = document.cookie.split(";");
-    for (i = 0; i < ARRcookies.length;i++) {
-        x = ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
-        y = ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
-        x = x.replace(/^\s+|\s+$/g,"");
-        if (x=='kltra')
-        {
+    for (i = 0; i < ARRcookies.length; i++) {
+        x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
+        y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
+        x = x.replace(/^\s+|\s+$/g, "");
+        if (x === 'kltra') {
             return unescape(y);
         }
     }
-}
+};
 
 
-if(o.CONST.url.indexOf('login') !== -1){ 
+if (o.CONST.url.indexOf('login') !== -1) {
 
-	if (o.CONST.cookie() != null && o.CONST.cookie() != "") { 
+	if (o.CONST.cookie() !== null && o.CONST.cookie() !== "") {
 		window.location = "/";
 	} else {
-		require(['Views/Login', 'Models/Login'], function() { 
-			$(document).ready(function(){
-				o.login = new o.Views.Login;
+		require(['Views/Login', 'Models/Login'], function () {
+            'use strict';
+			$(document).ready(function () {
+				o.login = new o.Views.Login();
 			});
 		});
 	}
-	
+
 } else {
 
-    if (o.CONST.cookie() != null && o.CONST.cookie() != "") {
-        require(['Views/Calendar', 'Models/Calendar', 
-				 'Views/Employee', 'Models/Employee', 
+    if (o.CONST.cookie() !== null && o.CONST.cookie() !== "") {
+        require(['Views/Calendar', 'Models/Calendar',
+				 'Views/Employee', 'Models/Employee',
 				 'Views/Project',  'Models/Project',
-				 'controller'], function() { 
-			$(document).ready(function(){
-				o.controller = new o.Controller();
-			});
+				 'controller'], function () {
+            'use strict';
+            $(document).ready(function () {
+            	o.controller = new o.Controller();
+            });
 		});
-        $('body').animate({ 
+        $('body').animate({
             opacity: 1,
             backgroundColor: "#d4d4d4"
         }, 500);
-		
+
     } else {
-        window.location = "/login"
+        window.location = "/login";
     }
 
 }
