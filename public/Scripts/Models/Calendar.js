9594da429fae20924bc1f61f91ba2407913f0cfa
Reed Rizzo <reedling78@gmail.com>
2012-12-07 11:13:13 -0600

    init

--- public/Scripts/Models/Calendar.js
+++ public/Scripts/Models/Calendar.js
@@ -13,7 +13,7 @@ o.Models.Calendar = Backbone.Model.extend({
 
 		for (var i = 0; i < dateMap.length; i++) {
 			var mapDate = new Date(dateMap[i].date);
-			if(mapDate == today){
+			if(mapDate.getMonth() == today.getMonth()){
 				console.log(dateMap[i]);
 			}
 		};
