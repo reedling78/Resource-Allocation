35beaa1eede57ad7972aa5ad6771c73204a6ef0b
Reed Rizzo <reedling78@gmail.com>
2012-12-07 10:37:15 -0600

    init

--- public/Scripts/Models/Calendar.js
+++ public/Scripts/Models/Calendar.js
@@ -17,7 +17,7 @@ o.Models.Calendar = Backbone.Model.extend({
 			, dayInc = 1
 
 			//roll back a month to account for current month
-			d.setMonth(d.getMonth() - 1);
+			d.setMonth(d.getMonth() - 2);
 
 		for (var m = this.get('monthCount') - 1; m >= 0; m--) {
 			var dayCount = 0;
